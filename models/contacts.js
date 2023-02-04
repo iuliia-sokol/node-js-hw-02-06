15a19f0413d7ef5cca6d926372073f52f2da60d7
Iuliia Sokol <avare779@gmail.com>
2023-02-04 10:22:45 +0200

    routes created

--- models/contacts.js
+++ models/contacts.js
@@ -1,8 +1,8 @@
-const { nanoid } = require("nanoid");
-const fs = require("fs/promises");
+const fs = require("fs").promises;
 const path = require("path");
+const { nanoid } = require("nanoid");
 
-const contactsPath = path.join(__dirname, "/models/", "contacts.json");
+const contactsPath = path.join(__dirname, "contacts.json");
 
 const listContacts = async () => {
   const data = await fs.readFile(contactsPath);
