acb7865e7857672125570b0cde737d7177de6c04
Iuliia Sokol <avare779@gmail.com>
2023-02-08 12:07:45 +0200

    decorators added

--- server.js
+++ server.js
@@ -8,12 +8,9 @@ mongoose
   .connect(DB_HOST)
   .then(() => {
     app.listen(PORT);
+    console.log("Database connection successful");
   })
   .catch((error) => {
     console.log(error.message);
     process.exit(1);
   });
-
-// app.listen(PORT, () => {
-//   console.log("Server running. Use our API on port: 3002");
-// });
